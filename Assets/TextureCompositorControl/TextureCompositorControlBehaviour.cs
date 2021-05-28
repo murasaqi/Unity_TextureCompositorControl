1c70b21ec5459df0c5508d0523a04fa010ed0f0c
murasaqi <uma9266@gmail.com>
2021-05-28 11:10:45 +0900

    fix initialize value

--- Assets/TextureCompositorControl/TextureCompositorControlBehaviour.cs
+++ Assets/TextureCompositorControl/TextureCompositorControlBehaviour.cs
@@ -8,11 +8,11 @@ public class TextureCompositorControlBehaviour : PlayableBehaviour
 {
     public bool wiggle;
     [HideInInspector][SerializeField] public Camera camera;
-    [SerializeField] public Vector2 noiseSeed;
-    [SerializeField] public Vector2 noiseScale;
-    [SerializeField] public float roughness;
-    [SerializeField] public Vector2 wiggleRange;
-    [SerializeField] public Vector2 offsetPosition;
+    [SerializeField] public Vector2 noiseSeed = Vector2.one;
+    [SerializeField] public Vector2 noiseScale = Vector2.one;
+    [SerializeField] public float roughness = 1;
+    [SerializeField] public Vector2 wiggleRange  = new Vector2(5,5);
+    [SerializeField] public Vector2 offsetPosition = Vector2.zero;
    
     public override void OnPlayableCreate (Playable playable)
     {
